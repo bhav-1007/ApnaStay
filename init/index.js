035593e364c7af2157aeaede6e09e0dd22ef936f
Bhavesh Bhatera <bhaveshbhatera1007@gmail.com>
2026-07-02 12:00:29 +0530

    Initial commit

---
+++ init/index.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const initData = require('./data.js');
+const Listing = require('../models/listing.js');
+
+main()
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/ApnaStay');
+}
+
+const initDB = async () => {
+  await Listing.deleteMany({});
+  await Listing.insertMany(initData.data);
+  console.log('Data was initialized');
+};
+
+initDB();
+
+module.exports = initDB;
---
+++ models/listing.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    filename: {
+      type: String,
+      default: "listingimage",
+    },
+    url: {
+      type: String,
+      default: "https://wallpaperaccess.com/full/8183887.jpg",
+      set: (v) =>
+        v === "" ? "https://wallpaperaccess.com/full/8183887.jpg" : v,
+    },
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+
+module.exports = Listing;
